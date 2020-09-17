385b84604f62e3f0d3962a7ca0d94530e75a9fdc
Carla Penna <carla.penna@fatec.sp.gov.br>
2020-09-17 19:42:44 -0300

    RegisterPage

--- lib/app/pages/registerpage/register_page.dart
+++ lib/app/pages/registerpage/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marcahorario/app/pages/login/login_page.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -6,10 +7,89 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  bool checkBoxValue = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.lime,
+    return Scaffold(
+      body: SingleChildScrollView(
+              child: Container(
+          height: 700,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration:
+                                InputDecoration(labelText: "Nome completo"),
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Digite um nome';
+                              }
+                              return null;
+                            },
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(labelText: "Telefone"),
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(labelText: "E-mail"),
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Digite um e-mail';
+                              }
+                              return null;
+                            },
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(labelText: "Senha"),
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Digite uma senha';
+                              }
+                              return null;
+                            },
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Checkbox(
+                                value: checkBoxValue,
+                                onChanged: (bool value) {
+                                  setState(() {
+                                    checkBoxValue = value;
+                                  });
+                                },
+                              ),
+                              Text("Termos e Condições")
+                            ],
+                          ),
+                        ],
+                      )),
+                ),
+              ),
+              Container(
+                  child: RaisedButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginPage()));
+                  }
+                },
+                child: Text("Criar Conta"),
+              ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
